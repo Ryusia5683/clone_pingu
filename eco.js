ca42dbdaa5fbc4af88f2a90f3d16b546fae9ebae
Ryusia5683 <115971861+Ryusia5683@users.noreply.github.com>
2022-11-02 18:30:12 +0900

    edit 4 resize

--- eco.js
+++ eco.js
@@ -2,69 +2,90 @@ const book = document.querySelector('.book');
 const btnNext = document.querySelector('.page_next');
 const btnPrev = document.querySelector('.page_prev');
 
-let bookImg = [
-    {
-        left : '01',
-        right : '02'
-    },
-    {
-        left : '03',
-        right : '04'
-    },
-    {
-        left : '05',
-        right : '06'
-    },
-    {
-        left : '07',
-        right : '08'
-    },
-    {
-        left : '09',
-        right : '10'
-    },
-    {
-        left : '11',
-        right : '12'
-    },
-    {
-        left : '13',
-        right : '14'
-    },
-    {
-        left : '15',
-        right : '16'
-    }
-];
-let curPage = '';
-bookImg.forEach( (element) => {
-    curPage += `<div>
-        <figure><img src="./img/ecology/p_${element.left}.gif" alt="p${element.left}"></figure>
-        <figure><img src="./img/ecology/p_${element.right}.gif" alt="p${element.right}}"></figure>
-    </div>`;
-})
-book.innerHTML = curPage;
+fetch( "./eco.json" )
+.then( (str) => ( str.json() ))
+.then( (data) => {
 
-const page = document.querySelectorAll('.book div');
-let idx = 0;
+    let viewWidth = window.innerWidth;
+    let curPage = '';
+    let pagePC;
+    let pageSM;
+    let idx = 0;
 
-page[0].classList.add('active');
-function nextPage(i) {
-    page[i-1].classList.remove('active');
-    page[i].classList.add('active');
-}
-function prevPage(i){
-    page[i].classList.remove('active');
-    page[i-1].classList.add('active');
-}
+    // resize시 출력 형태 변경
+    window.addEventListener('resize', function(){ 
+        viewWidth = window.innerWidth;
+        printBook();
+    })
 
-btnNext.addEventListener('click', function(){
-    if(idx < page.length-1){
-        nextPage(++idx);
+    function printBook(){
+        idx = 0;
+        curPage = '';
+        if(viewWidth > 500){
+            // book PC 출력
+            data.book_PC.forEach( (obj, idx) => { 
+                curPage += `<div class="PC">
+                <figure><img src="./img/ecology/${obj.left}" alt="p${idx}_left"></figure>
+                <figure><img src="./img/ecology/${obj.right}" alt="p${idx}_right}"></figure>
+                </div>`;
+            })
+            book.innerHTML = curPage;
+        } else if(viewWidth <= 500){ 
+            // book mobile 출력
+            data.book_SM.forEach( (obj, idx) => { 
+                curPage += `<div class="SM">
+                <figure><img src="./img/ecology/${obj.img}" alt="p${idx}"></figure>
+                </div>`;
+            })
+            book.innerHTML = curPage;
+        }
+        
+        pagePC = document.querySelectorAll('.book .PC');
+        pageSM = document.querySelectorAll('.book .SM');
+
+        // 초기 화면 출력
+        if(viewWidth > 500){
+            pagePC[0].classList.add('active');
+        } else if(viewWidth <= 500) {
+            pageSM[0].classList.add('active');
+        }
+        
     }
-})
-btnPrev.addEventListener('click', function(){
-    if(idx > 0){
-        prevPage(idx--);
+    printBook();
+
+    // 페이지 전환 함수
+    function nextPagePC(i) {
+        pagePC[i-1].classList.remove('active');
+        pagePC[i].classList.add('active');
     }
+    function prevPagePC(i){
+        pagePC[i].classList.remove('active');
+        pagePC[i-1].classList.add('active');
+    }
+
+    function nextPageSM(i){
+        pageSM[i-1].classList.remove('active');
+        pageSM[i].classList.add('active');
+    }
+    function prevPageSM(i){
+        pageSM[i].classList.remove('active');
+        pageSM[i-1].classList.add('active');
+    }
+
+    // 버튼 기능
+    btnNext.addEventListener('click', function(){
+        if(idx < pagePC.length-1 && viewWidth > 500){
+            nextPagePC(++idx);
+        } else if ( idx < pageSM.length - 1 && viewWidth <= 500){
+            console.log(idx, pageSM.length-1)
+            nextPageSM(++idx);
+        }
+    })
+    btnPrev.addEventListener('click', function(){
+        if(idx > 0 && viewWidth > 500){
+            prevPagePC(idx--);
+        } else if ( idx > 0 && viewWidth <= 500){
+            prevPageSM(idx--);
+        }
+    })
 })
